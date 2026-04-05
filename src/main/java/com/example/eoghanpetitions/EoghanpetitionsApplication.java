94a542063bd11fed51925cbc2bf5a007abe41e34
EoghanPetition <e.fallon1@universityofgalway.ie>
2026-04-05 16:46:15 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/eoghanpetitions/EoghanpetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.eoghanpetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EoghanpetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EoghanpetitionsApplication.class, args);
+	}
+
+}
